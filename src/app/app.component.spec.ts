3354e51452dad0f63e9ecd7dc2d8a33b37836fe7
horaci <horacimail@gmail.com>
2023-03-08 17:56:54 +0100

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -26,10 +22,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('ready2travel');
   });
 
-  it('should render title', () => {
+  it('should render title on header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ready2travel app is running!');
+    expect(compiled.querySelector('span')?.textContent).toContain(
+      'Ready2Travel'
+    );
   });
 });
