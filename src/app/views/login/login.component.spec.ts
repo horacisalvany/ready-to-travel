473cb8a74b80f20d140636d320e378a6a9266c75
Horaci <horacimail@gmail.com>
2026-03-01 20:18:22 +0100

    fix tests

--- src/app/views/login/login.component.spec.ts
+++ src/app/views/login/login.component.spec.ts
@@ -1,15 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Auth } from '@angular/fire/auth';
+import { of } from 'rxjs';
+import { AuthService } from '../../services/auth.service';
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
-  const mockAuth = {
-    currentUser: null,
+  const mockAuthService = {
+    user$: of(null),
+    isAuthenticated$: of(false),
+    login: jasmine.createSpy('login').and.returnValue(Promise.resolve()),
+    loginWithGoogle: jasmine.createSpy('loginWithGoogle').and.returnValue(Promise.resolve()),
+    register: jasmine.createSpy('register').and.returnValue(Promise.resolve()),
+    logout: jasmine.createSpy('logout').and.returnValue(Promise.resolve()),
   };
 
   beforeEach(async () => {
@@ -19,7 +25,7 @@ describe('LoginComponent', () => {
         BrowserAnimationsModule,
         RouterTestingModule,
       ],
-      providers: [{ provide: Auth, useValue: mockAuth }],
+      providers: [{ provide: AuthService, useValue: mockAuthService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
